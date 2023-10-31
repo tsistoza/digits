7799fe7491690d9e1cc070f21b8f3a12b7492e4a
Vinton Sistoza <123343040+tsistoza@users.noreply.github.com>
2023-10-30 21:09:22 -1000

    Added contacts collection, as changed publications, chaged listcontacts to use the collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/contact/Contact';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,22 @@ const addData = (data) => {
   Stuffs.collection.insert(data);
 };
 
+const addDataToContacts = (data) => {
+  console.log('Adding data to contacts');
+  Contacts.collection.insert(data);
+};
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating Contact data.');
+    Meteor.settings.defaultContacts.forEach(data => addDataToContacts(data));
   }
 }
