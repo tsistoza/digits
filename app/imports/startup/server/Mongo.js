81a57ebab018eecf15f8291f5b6143f391bc4722
Vinton Sistoza <tsistoza@gmail.com>
2023-11-04 16:49:14 -1000

    cleanup crew

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,27 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Contacts } from '../../api/contact/Contact';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
-
 const addDataToContacts = (data) => {
   console.log('Adding data to contacts');
   Contacts.collection.insert(data);
 };
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 
 if (Contacts.collection.find().count() === 0) {
   if (Meteor.settings.defaultContacts) {
